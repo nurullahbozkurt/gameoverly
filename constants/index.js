aaa7163fdbd4d8d6c0e90104f119e6f275fa930d
nurullahbozkurt <nurullahbozkurtt@gmail.com>
2023-03-10 18:31:38 +0300

    first

---
+++ constants/index.js
@@ -0,0 +1,87 @@
+export const exploreWorlds = [
+  {
+    id: "world-1",
+    imgUrl: "/gameoverly.jpeg",
+    title: "I'm Racer",
+  },
+  {
+    id: "world-2",
+    imgUrl: "/gameoverly1.jpeg",
+    title: "I'm Fighter",
+  },
+  {
+    id: "world-3",
+    imgUrl: "/gameoverly2.jpeg",
+    title: "I'm Manager",
+  },
+  {
+    id: "world-4",
+    imgUrl: "/gameoverly3.jpeg",
+    title: "I'm MMORPG player",
+  },
+  {
+    id: "world-5",
+    imgUrl: "/gameoverly4.jpeg",
+    title: "I'm strategy gamer",
+  },
+];
+
+export const startingFeatures = [
+  "Find a world that suits you and you want to enter",
+  "Get to know the players",
+  "No need to beat around the bush, just stay on the gas and have fun",
+];
+
+export const newFeatures = [
+  {
+    imgUrl: "/vrpano.svg",
+    title: "Know your competitors.",
+    subtitle: "Now you will know your competitors.",
+  },
+  {
+    imgUrl: "/headset.svg",
+    title: "Get ready to fight.",
+    subtitle: "Collaborate before challenge.",
+  },
+];
+
+export const insights = [
+  {
+    imgUrl: "/gameoverly5.jpeg",
+    title: "Fortnite Introduces New Game Mode: The Spy Within",
+    subtitle:
+      " Fortnite, one of the most popular battle royale games, has recently introduced a new game mode called `The Spy Within`. This mode pits players against each other in a game of deception and sabotage, as players try to uncover who the spy is among their team while completing tasks to earn points and avoid elimination.",
+  },
+  {
+    imgUrl: "/gameoverly6.webp",
+    title:
+      "League of Legends Championship Finals Set to Take Place in Shanghai",
+    subtitle:
+      "The annual League of Legends Championship Finals, one of the biggest esports events in the world, is set to take place in Shanghai, China. Top teams from around the globe will compete for the championship title and a prize pool of millions of dollars.",
+  },
+  {
+    imgUrl: "/gameoverly7.webp",
+    title: "Minecraft Surpasses 200 Million Copies Sold Worldwide",
+    subtitle:
+      "Minecraft, the sandbox game that allows players to build and explore their own virtual worlds, has surpassed 200 million copies sold worldwide. The game's popularity shows no signs of slowing down, as new updates and features continue to be added to keep players engaged and excited.",
+  },
+];
+
+export const socials = [
+  {
+    name: "twitter",
+    url: "/twitter.svg",
+  },
+  {
+    name: "linkedin",
+    url: "/linkedin.svg",
+  },
+  {
+    name: "instagram",
+    url: "/instagram.svg",
+  },
+  {
+    name: "facebook",
+    url: "/facebook.svg",
+  },
+];
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,21 @@
-import '../styles/globals.css'
+import Head from "next/head";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+import "../styles/globals.css";
 
-export default MyApp
+const MyApp = ({ Component, pageProps }) => (
+  <>
+    <Head>
+      <title>Gameoverly</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link rel="icon" href="/gaming.png" />
+      <link rel="preconnect" href="https://stijndv.com" />
+      <link
+        rel="stylesheet"
+        href="https://stijndv.com/fonts/Eudoxus-Sans.css"
+      />
+    </Head>
+    <Component {...pageProps} />
+  </>
+);
+
+export default MyApp;
